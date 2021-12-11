0ed9fba1b51b28882415f4f825ed3fc083893f59
Uchiha-Shisui <aleqs.amir@gmail.com>
2021-12-11 21:42:07 +0400

    first_lesson

---
+++ das64_forme/index.php
@@ -0,0 +1,14 @@
+<?php
+    $a = "text";
+//    echo $a;
+    $arr = [1, 2, "ole", 4, 5, 6];
+//    $sum = $arr[0]+$arr[1]+$arr[3]+$arr[4]+$arr[5];
+//    echo $sum
+    $arr2 = [
+        "login" => "aa123",
+        "password" => "123456",
+        "name" => "Alex"
+    ];
+//    echo $arr2["name"];
+    echo '<pre>';
+    var_dump($arr);
