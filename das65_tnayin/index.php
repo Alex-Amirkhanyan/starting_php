d7968fcfd0e5883ec3ef3a5a47e596bd9278f6b5
Uchiha-Shisui <aleqs.amir@gmail.com>
2021-12-14 19:07:20 +0400

    get_num"3"_from_num"1024"

---
+++ das65_tnayin/index.php
@@ -0,0 +1,20 @@
+<?php
+    //Errord tarberak
+//    $a = sqrt(1024);
+//    $b = round(sqrt($a));
+//    $c = round(sqrt($b)+0.1);
+//    echo $c;
+
+    //Erkrord tarberak;
+//    const main_num = 1024;
+//    for ($i = 1024; $i > 0; $i--) {
+//        if (main_num-$i == 3) {
+//            echo main_num-$i;
+//        }
+//    }
+
+    //Arajin Tarberak;
+//    $num = 1024;
+//    $rand_num = rand(1021,1021);
+//    $result = $num - $rand_num;
+//    echo $result;
