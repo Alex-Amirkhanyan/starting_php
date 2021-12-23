136ac9033112c7b2ef28e7f6cd04a908c923953d
Uchiha-Shisui <aleqs.amir@gmail.com>
2021-12-23 10:52:40 +0400

    exercises

---
+++ das68/index.php
@@ -0,0 +1,123 @@
+<?php
+    // $arr = ["red", "red", "blue", "green", "orange"];
+    // $result = array_unique($arr);
+    // echo '<pre>';
+    // var_dump($result);
+
+
+
+    // $str = "three one two";
+    // function action($arg) {
+    //     $arg = explode(" ", $arg);
+    //     $new_str = "";
+    //     for($i = 0; $i <= count($arg) - 1; $i++) {
+    //         switch($arg[$i]) {
+    //             case "one": 
+    //                 $new_str = $new_str . 1;
+    //                 break;
+    //             case "two": 
+    //                 $new_str .= 2;
+    //                 break;
+    //             case "three": 
+    //                 $new_str .= 3;
+    //                 break;
+    //         }
+    //     }
+    //     return $new_str;
+    // }
+    // echo action($str);
+
+
+
+    // $arr = ["one", "three", "two"];
+
+    
+    // for($i = 0; $i < count($arr); $i++) {
+    //     switch($arr[$i]) {
+    //         case "one": 
+    //             $arr[$i] = 1;
+    //             break;
+    //         case "two": 
+    //             $arr[$i] = 2;
+    //             break;
+    //         case "three": 
+    //             $arr[$i] = 3;
+    //             break;
+    //     }
+    // }
+    // echo '<pre>';
+    // var_dump($arr);
+
+
+
+    // $nums = [1, 2, 3, 4, 5];
+    
+    // function numbers($arg){
+    //     $res = 0;
+    //     for($i = 0; $i < count($arg); $i++) {
+    //         if($arg[$i] % 2 == 0) {
+    //             $res += $arg[$i];
+    //         }
+    //     }    
+    //     return $res;
+    // }
+    // echo numbers($nums);
+
+
+
+
+    // $arr2 = [123, 57, 47, 63, 48, 93, 45, 64];
+    // $new_arr = [];
+    // for($i = 0; $i < count($arr2); $i++) {
+    //     if($arr2[$i] % 3 == 0) {
+    //         array_push($new_arr, $arr2[$i]);
+    //     }
+    // }
+    // echo '<pre>';
+    // var_dump($new_arr);
+
+
+
+    // $a = 5;
+    // $b = 7;
+    // $a = ($a + $b);
+    // $b = $a - $b;
+    // $a = $a - $b;
+    // echo $a . '<br>';
+    // echo $b;
+
+
+    // $email = "some@address.com";
+
+    // function check_email($arg) {
+    //     if(filter_var($arg, FILTER_VALIDATE_EMAIL)) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
+    // $res = check_email($email);
+    // if($res) {
+    //     echo "Chisht e";
+    // }else {
+    //     echo "sxal e";
+    // }
+
+
+    
+
+    
+    // $givenArray = array(2,5,2,10,4,5,8,16);
+    // $uniqueArry = array();
+ 
+    // foreach($givenArray as $val) {
+    //     foreach($uniqueArry as $uniqueValue) {
+    //         if($val == $uniqueValue) {
+    //             continue 2;
+    //         }
+    //     }
+    //     $uniqueArry[] = $val;
+    // }
+    // echo '<pre>';
+    // var_dump($uniqueArry);
